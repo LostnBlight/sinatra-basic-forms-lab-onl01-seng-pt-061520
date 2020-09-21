60236989e995f7963920288bc3b1c5fba558f748
Cameron Hudson <camnn3@yahoo.com>
2020-09-21 20:14:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
   
+  get '/' do
+    erb :index
+  end
+  
+  get '/new' do
+    erb :create_puppy
+  end
+  
+  post '/pupp'
 end
